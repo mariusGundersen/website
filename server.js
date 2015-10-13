5dd3885db56a396c8267f51f77134794d8cafb41
Marius Gundersen <me@mariusgundersen.net>
2015-10-13 22:08:57 +0200

    compressing with gzip

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const static = require('express-static');
+const compression = require('compression');
 const app = express();
+
+app.use(compression());
 
 app.use(static('output', {
   etag: false,
