39cdf40e43000f74f69c35105a06578ce4de960f
Marius Gundersen <me@mariusgundersen.net>
2015-10-13 22:03:33 +0200

    cache static content for one day

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const static = require('express-static');
 const app = express();
 
-app.use(static('output'));
+app.use(static('output', {
+  etag: false,
+  lastModified: false,
+  maxAge: '1d'
+}));
 
 app.use((req, res, next) => res.sendFile(__dirname+'/output/404.html'));
 
